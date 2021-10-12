0fed139dcc41417f64d29acf816a749f92171ace
eunhyung-lee <88265446+eunhyung-lee@users.noreply.github.com>
2021-10-12 23:00:34 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ const gs_creds = require("./credentials.json");
 const doc = new GoogleSpreadsheet(
   "1uT0nKutfx6ogmquFBoyJzsqLQHKkluX42M3WUE5KwBQ"
 );
+const STOCK_COMPOSITE = "StockComposite";
+const ASSET_HISTORY = "AssetHistory";
 
 async function authGoogleSheet() {
-  console.log("Auth start");
   try {
     await doc.useServiceAccountAuth(gs_creds);
-    // await doc.loadInfo();
   } catch (err) {
     console.log("AUTH ERROR", err);
   }
-  console.log("Auth end");
 }
 authGoogleSheet();
 
-async function readFirstSheet() {
-  console.log("data road start");
+async function readSheet() {
+  // console.log("data road start");
 
   await doc.loadInfo(); // loads document properties and worksheets
-  var sheet = doc.sheetsByIndex[0];
+  let stockSheet = doc.sheetsByTitle(STOCK_COMPOSITE);
+  let assetSheet = doc.sheetsById(ASSET_HISTORY);
   // const rows = await sheet.getRows();
   // console.log(rows.length);
   // console.log(rows[0].date);
@@ -29,17 +29,13 @@ async function readFirstSheet() {
   // rows[0].test2 = 111;
   // await rows[0].save();
 
-  var rows = await sheet.getRows({ offset: 0, limit: 4 });
+  var rows = await assetSheet.getRows({ offset: 0, limit: 4 });
   rows.forEach((ele) => {
-    // console.log(rows.length);
-    // console.log(count);
-    // count++;
     console.log(ele._rawData[0], ele._rawData[1], ele._rawData[2]);
   });
   // console.log(doc.title);
   // console.log(doc.sheetsByIndex[0].title);
   // console.log(doc.sheetsByIndex[0].rowCount);
-
-  console.log("data road end");
+  // console.log("data road end");
 }
-readFirstSheet();
+readSheet();
