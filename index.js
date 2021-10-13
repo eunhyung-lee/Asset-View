d492337da636977bb8224ee638a9bdf8bdd20322
eunhyung-lee <88265446+eunhyung-lee@users.noreply.github.com>
2021-10-13 22:19:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,22 +5,34 @@ const doc = new GoogleSpreadsheet(
 );
 const STOCK_COMPOSITE = "StockComposite";
 const ASSET_HISTORY = "AssetHistory";
+const ASSET_SUM = "AssetSum";
+const STOCK_LOCAL = "StockLocal";
+const STOCK_OVERSEA = "StockOversea";
+const REITS = "Reits";
+const CASH_THINGS = "CashThings";
 
 async function authGoogleSheet() {
+  console.log("auth start");
   try {
     await doc.useServiceAccountAuth(gs_creds);
   } catch (err) {
     console.log("AUTH ERROR", err);
   }
+  console.log("auth end");
 }
 authGoogleSheet();
 
 async function readSheet() {
-  // console.log("data road start");
+  console.log("data road start");
 
   await doc.loadInfo(); // loads document properties and worksheets
-  let stockSheet = doc.sheetsByTitle(STOCK_COMPOSITE);
-  let assetSheet = doc.sheetsById(ASSET_HISTORY);
+  let stockSheet = doc.sheetsByTitle[STOCK_COMPOSITE];
+  let assetSheet = doc.sheetsByTitle[ASSET_HISTORY];
+
+  let rows = await assetSheet.getRows({ offset: 0, limit: 4 });
+  rows.forEach((ele) => {
+    console.log(ele._rawData[0], ele._rawData[1], ele._rawData[2]);
+  });
   // const rows = await sheet.getRows();
   // console.log(rows.length);
   // console.log(rows[0].date);
@@ -29,13 +41,10 @@ async function readSheet() {
   // rows[0].test2 = 111;
   // await rows[0].save();
 
-  var rows = await assetSheet.getRows({ offset: 0, limit: 4 });
-  rows.forEach((ele) => {
-    console.log(ele._rawData[0], ele._rawData[1], ele._rawData[2]);
-  });
   // console.log(doc.title);
   // console.log(doc.sheetsByIndex[0].title);
   // console.log(doc.sheetsByIndex[0].rowCount);
-  // console.log("data road end");
+  console.log("data road end");
 }
 readSheet();
+console.log("program end");
